fa160db3207e4e36b26fe28c0b5ab58f807485d8
Serhat Arslan <49308319+serhatx1@users.noreply.github.com>
2024-05-09 00:12:07 +0300

    First

---
+++ src/json/.js
@@ -0,0 +1,72 @@
+import img1 from "../assets/1.png"
+import img2 from "../assets/2.png"
+import img3 from "../assets/3.png"
+import img4 from "../assets/4.png"
+import img5 from "../assets/5.png"
+import img6 from "../assets/6.png"
+import img7 from "../assets/7.png"
+
+export default [
+    {
+        "id":1,
+        "img":img1,
+        "cost":"40$",
+        "name":"M4A1-S | Imminent Danger",
+        "bg":"purple",
+        "percent":"20"
+    },
+    {
+        "id":2,
+        "img":img2,
+        "cost":"140$",
+        "name":"AK-47 | Gold Arabesque",
+        "bg":"purple",
+        "percent":"5"
+
+    },
+    {
+        "id":3,
+        "img":img3,
+        "cost":"25$",
+        "name":"StatTrak™ AUG | Torque",
+        "bg":"gray",
+        "percent":"20"
+
+    },
+    {
+        "id":4,
+        "img":img4,
+        "cost":"250$",
+        "name":"StatTrak™ AK-47 | Ice Coaled",
+        "bg":"red",
+        "percent":"10"
+
+    },
+    {
+        "id":5,
+        "img":img5,
+        "cost":"7850$",
+        "name":"Sport Gloves | Vice",
+        "bg":"yellow",
+        "percent":"7"
+
+    },
+    {
+        "id":6,
+        "img":img6,
+        "cost":"1200$",
+        "name":"Karambit | Fade",
+        "bg":"yellow",
+        "percent":"2"
+
+    },
+    {
+        "id":7,
+        "img":img7,
+        "cost":"2$",
+        "name":"Dual Berettas | Cobra Strike",
+        "bg":"gray",
+        "percent":"36"
+
+    }
+]
